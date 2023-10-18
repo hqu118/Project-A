e24cfe6c4aaa37ec81f0603eb193c995d5d30c00
hqu118 <hqu118@aucklanduni.ac.nz>
2023-10-18 18:02:49 +1300

    Update GraphControl.java

--- main/GraphControl.java
+++ main/GraphControl.java
@@ -10,4 +10,8 @@ public class GraphControl {
     public GraphControl() {
         sUI = new GraphUI();
     }
+
+    private void createGraph() {
+        graph = new Graph(sUI.getRelationElements(), sUI.getWeightElements());
+    }
 }
