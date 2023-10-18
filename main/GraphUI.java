19606240ae64e4a710baff5623dbce18b4ef12b4
hqu118 <hqu118@aucklanduni.ac.nz>
2023-10-18 17:54:39 +1300

    Update GraphUI.java

--- main/GraphUI.java
+++ main/GraphUI.java
@@ -1,6 +1,10 @@
 package main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Scanner;
 
@@ -35,4 +39,85 @@ public class GraphUI {
         System.out.println("----------------------------------------------------------------------");
     }
 
+    private boolean processFile() {
+        fileLines.clear();
+        try {
+            File f = new File(fileName);
+            if (!f.exists()) {
+                setFileStatusFalse();
+                return false;
+            } else {
+                setFileStatusTrue();
+            }
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                fileLines.add(s.nextLine());
+            }
+            s.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File reading error");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public void setFileStatusTrue() {
+        fileStatus = true;
+    }
+
+    public void setFileStatusFalse() {
+        fileStatus = false;
+    }
+
+    // Other getters and setters
+    public void setFileName(String file) {
+        fileName = createFileName(file);
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public List<String> getWeightElements() {
+        return weightElements;
+    }
+
+    public boolean getFileStatus() {
+        return fileStatus;
+    }
+
+    public List<String> getSetElements() {
+        return setElements;
+    }
+
+    public List<String> getRelationElements() {
+        return relationElements;
+    }
+
+    private String createFileName(String file) {
+        String line = System.getProperty("user.dir");
+        System.out.println("The current directory is " + line);
+        line = line + File.separator + "testcases" + File.separator + file;
+        System.out.println("The full path name is: " + line);
+        return line;
+    }
+
+    private void listSetMembers() {
+        System.out.println("The set elements are: " + concatenateElements(Collections.enumeration(setElements)));
+    }
+
+    private String concatenateElements(Enumeration<String> elements) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        while (elements.hasMoreElements()) {
+            String nextEl = (String) elements.nextElement();
+            sb.append(nextEl);
+            if (elements.hasMoreElements()) {
+                sb.append(",");
+            }
+        }
+        sb.append("}");
+        return sb.toString();
+    }
 }
