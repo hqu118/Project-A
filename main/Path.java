320cd6f5bc25ba4d40f841b3f4d70446c341cd79
hqu118 <hqu118@aucklanduni.ac.nz>
2023-10-18 17:47:35 +1300

    Update Path.java

--- main/Path.java
+++ main/Path.java
@@ -16,4 +16,25 @@ public class Path {
         this.path = new ArrayList<>(list);
         this.totalCost = totalCost;
     }
+
+    public List getPath() {
+        return path;
+    }
+
+    public int getTotalCost() {
+        return totalCost;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < path.size(); i++) {
+            sb.append(path.get(i).getValue());
+            if (i != path.size() - 1 && path.size() != 1) {
+                sb.append(" -> ");
+            }
+        }
+        sb.append(" cost: " + totalCost);
+        return sb.toString();
+    }
 }
